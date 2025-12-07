6ac51f29772b50e15828a8ee7731c06d3599702f
shri030306 <shrilakshmigs03@gmail.com>
2025-12-07 19:09:26 +0530

    save

---
+++ employee.py
@@ -0,0 +1,15 @@
+def employee_details(name, emp_id, department, salary):
+    result = (
+        f"Employee Name: {name}\n"
+        f"Employee ID: {emp_id}\n"
+        f"Department: {department}\n"
+        f"Salary: {salary}"
+    )
+    return result
+
+if __name__ == "__main__":
+    name = "Shreesai"
+    emp_id = "0318"
+    department = "HR"
+    salary = 56000
+    print(employee_details(name, emp_id, department, salary))
